afb4c783407088d156e1d6fa83017ee3154bb147
Jeremy Viallatt <shoo>
2023-06-21 11:21:21 +0200

    Extract radar div to his own component

--- src/components/PokedexRightPanel.vue
+++ src/components/PokedexRightPanel.vue
@@ -1,87 +1,18 @@
 <script setup lang="ts">
 import PokemonTypeList from './PokemonTypeList.vue'
 import { PokemonType } from '../types/Pokemon.ts'
-import { computed } from 'vue'
+import PokemonStatRadarChart from './PokemonStatRadarChart.vue'
 
-const props = defineProps<{
+defineProps<{
   pokemonCurrentStat: number[],
   pokemonCurrentType: { type: PokemonType[], default: [] }
 }>()
-
-const chartSeries = computed(() => [{
-  name: 'Stat',
-  data: props.pokemonCurrentStat ?? []
-}])
-
-const chartOptions = {
-  chart: {
-    type: 'radar',
-    toolbar: {
-      show: false
-    }
-  },
-  markers: {
-    size: 4,
-    colors: ['#fff'],
-    strokeColor: '#FF4560',
-    strokeWidth: 2
-  },
-  tooltip: {
-    enabled: false
-  },
-  xaxis: {
-    categories: [
-      'HP',
-      'ATK',
-      'DEF',
-      'S-ATK',
-      'S-DEF',
-      'SPD'
-    ],
-    labels: {
-      style: {
-        colors: ['#000000', '#000000', '#000000', '#000000', '#000000', '#000000']
-      }
-    }
-  },
-  yaxis: {
-    max: 255,
-    tickAmount: 7,
-    labels: {
-      formatter: (val: string, i: number) => (i % 2 === 0) ? val : ''
-    },
-    forceNiceScale: true
-  },
-  dataLabels: {
-    enabled: true
-  },
-  plotOptions: {
-    radar: {
-      size: 110,
-      polygons: {
-        strokeColors: '#e9e9e9',
-        fill: {
-          colors: ['#f8f8f8', '#fff']
-        }
-      }
-    }
-  },
-  colors: ['#FF4560']
-}
 
 </script>
 
 <template>
   <section class="pokedex-right-panel">
-    <div class="pokedex-right-panel__chart">
-      <apexchart
-        type="radar"
-        height="350"
-        :options="chartOptions"
-        :series="chartSeries"
-      />
-    </div>
-
+    <PokemonStatRadarChart :pokemon-current-stat="pokemonCurrentStat" />
     <PokemonTypeList :current-pokemon-type="pokemonCurrentType" />
   </section>
 </template>
@@ -90,7 +21,6 @@ const chartOptions = {
 @use "../style.scss" as *;
 
 .pokedex-right-panel {
-
   position: relative;
   display: flex;
   flex-direction: column;
@@ -114,14 +44,6 @@ const chartOptions = {
       border-right: 100px solid transparent;
       border-bottom: 70px solid var(--color-background-red-dark);
     }
-  }
-
-  &__chart {
-    width: inherit;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    align-content: center;
   }
 }
 
---
+++ src/components/PokemonStatRadarChart.vue
@@ -0,0 +1,89 @@
+<script setup lang="ts">
+import { computed } from 'vue'
+
+const props = defineProps<{
+  pokemonCurrentStat: number[]
+}>()
+
+const chartSeries = computed(() => [{
+  name: 'Stat',
+  data: props.pokemonCurrentStat ?? []
+}])
+
+const chartOptions = {
+  chart: {
+    type: 'radar',
+    toolbar: {
+      show: false
+    }
+  },
+  markers: {
+    size: 4,
+    colors: ['#fff'],
+    strokeColor: '#FF4560',
+    strokeWidth: 2
+  },
+  tooltip: {
+    enabled: false
+  },
+  xaxis: {
+    categories: [
+      'HP',
+      'ATK',
+      'DEF',
+      'S-ATK',
+      'S-DEF',
+      'SPD'
+    ],
+    labels: {
+      style: {
+        colors: ['#000000', '#000000', '#000000', '#000000', '#000000', '#000000']
+      }
+    }
+  },
+  yaxis: {
+    max: 255,
+    tickAmount: 7,
+    labels: {
+      formatter: (val: string, i: number) => (i % 2 === 0) ? val : ''
+    },
+    forceNiceScale: true
+  },
+  dataLabels: {
+    enabled: true
+  },
+  plotOptions: {
+    radar: {
+      size: 110,
+      polygons: {
+        strokeColors: '#e9e9e9',
+        fill: {
+          colors: ['#f8f8f8', '#fff']
+        }
+      }
+    }
+  },
+  colors: ['#FF4560']
+}
+</script>
+
+<template>
+  <div class="pokedex-stat-radar-chart">
+    <apexchart
+      type="radar"
+      height="350"
+      :options="chartOptions"
+      :series="chartSeries"
+    />
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.pokedex-stat-radar-chart {
+  width: inherit;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  align-content: center;
+}
+</style>
